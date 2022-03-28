9450ed1a6e86fe9efea0898dbf6d4cef12ca366e
Darion <pixelarraymedia@gmail.com>
2022-03-28 10:42:37 -0700

    fetching items from databse, postman next

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,50 @@
+import express from 'express'
+const router = express.Router()
+import AsyncHandler from 'express-async-handler'
+
+import Product from '../models/productModel.js'
+
+
+
+// @desc Fetch all products
+// @route GET /api/products
+// @access Fetch all products
+
+router.get('/',
+
+AsyncHandler(async (req, res) => {
+
+    const products = await Product.find({})
+
+    res.json(products)
+})
+)
+
+
+// @desc Fetch all products
+// @route GET /api/products
+// @access Fetch all products
+
+router.get('/:id', AsyncHandler (async (req, res) => {
+    const product = await Product.findById(req.params.id)
+
+
+
+    if(product){
+   
+    res.json(product)
+    } else {
+
+        res.status(404).json({message: 'product not found'})
+    }
+ 
+})
+)
+
+
+
+
+export default router
+
+
+
--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,11 @@ import express from 'express'
 import dotenv from 'dotenv'
 import colors from 'colors'
 import connectDB from './config/db.js'
-import products from './data/products.js'
+
+
+import productRoutes from './routes/productRoutes.js'
+
+
 
 
 
@@ -16,6 +20,8 @@ const app = express()
 app.get('/', (req, res) => {
     res.send('API is running...')
 })
+
+app.use('/api/products', productRoutes)
 
 app.get('/api/products', (req, res) => {
     res.json(products)
