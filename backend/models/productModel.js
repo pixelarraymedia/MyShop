609036d1b1a3603177d775a51fa538486c6c51cf
Darion <pixelarraymedia@gmail.com>
2022-03-28 10:42:37 -0700

    fetching items from databse, postman next

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -1,15 +1,21 @@
 import mongoose from 'mongoose'
 
 
-const reviewSchema = mongoose.Schema ({
-    name: { type: String, required: true },
-    rating: { type: Number, required: true },
-    comment: { type: String, required: true }
-},  {
-    timestamps: true
-
-
-})
+const reviewSchema = mongoose.Schema (
+    {
+        name: { type: String, required: true },
+        rating: { type: Number, required: true },
+        comment: { type: String, required: true },
+        user: {
+          type: mongoose.Schema.Types.ObjectId,
+          required: true,
+          ref: 'User',
+        },
+      },
+      {
+        timestamps: true,
+      }
+    )
 
 const productSchema = mongoose.Schema (
     {
@@ -26,7 +32,7 @@ const productSchema = mongoose.Schema (
             required: true
         },
 
-        ImageData:{
+        image:{
             type: String,
             required: true
         },
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,92 +1,38 @@
 import mongoose from 'mongoose'
+import bcrypt from 'bcryptjs'
 
-const orderSchema = mongoose.Schema (
-    {
-        user:{
-            type: mongoose.Schema.Types.ObjectId,
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+        
+        email:{
+            type: String,
             required: true,
-            ref: 'User'
+            unique: true
         },
 
         
-        orderItems:[ 
-            {
-                name: { type: String, required: true},
-                qty: { type: Number, required: true},
-                image: { type: String, required: true},
-                price: { type: Number, required: true},
-                product: { 
-                    type: mongoose.Schema.Types.ObjectId,
-                    required: true,
-                    ref: 'Product'
-                },
-
-            }
-        ],
-
-        shippingAddress: {
-            address: {
-                type: String, required: true },
-                city:{ type: String, required : true },
-                postalCode: { type: String, required : true },
-                country : { type: String, required: true }
-            },
-
-            paymentMethod:{
-                type: String,
-                required: true,
-            },
-            paymentResult:{
-                id: { type: String},
-                status: { type: String},
-                update_time: { type: String},
-                email_address: { type: String},
-
-            
-            },   taxPrice:{
-                type: Number,
-                required: true,
-                defualt: 0.0
-            
-            },
-            shippingPrice:{
-                type: Number,
-                required: true,
-                defualt: 0.0
-            
-            },
-            totalPrice:{
-                type: Number,
-                required: true,
-                defualt: 0.0
-            
-            },
-            isPaid:{
-                type: Booolean,
-                required: true,
-                defualt: false
-            
-            },
-            paidAt:{
-                type: Date,
-       
-            },
-
-            isDelivered: {
-            type:Boolean,
-            required: true,
-            defualt:false,
-            },
-            deliveredAt: {
-            type:Date,
-                },
-
+        password:{
+            type: String,
+            required: true
         },
-{
-        timestamps: true,
+
+        
+        isAdmin :{
+            type: Boolean,
+            required: true,
+            default: false
+        }
+
+
+    }, {
+        timestamps: true
     }
 )
 
-const Order = mongoose.model('Order', orderSchema)
+const User = mongoose.model('User', userSchema)
 
-export default Order
+export default User
