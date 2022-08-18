db51908b4056125b6a3a7296ae3e2f2b69aba97b
Darion <pixelarraymedia@gmail.com>
2022-08-17 18:09:51 -0700

    Prep for deployment

--- backend/server.js
+++ backend/server.js
@@ -22,15 +22,10 @@ const app = express()
 if(process.env.NODE_ENV === 'development'){
     app.use(morgan('dev'))
     
-
 }
 
 app.use(express.json())
 
-
-app.get('/', (req, res) => {
-    res.send('API is running...')
-})
 
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
@@ -41,9 +36,21 @@ app.get('/api/config/paypal', (req, res) => res.send(process.env.PAYPAL_CLIENT_I
 const __dirname = path.resolve()
 app.use('/uplaods', express.static(path.join(__dirname, '/uploads')))
 
+if(process.env.NODE_ENV === 'production') {
+
+    app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+    app.get('*', (req, res) => 
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html' )))
+}  else {   
+app.get('/', (req, res) => {
+    res.send('API is running...')
+})
+}
+
 app.use(notFound)
 app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT, console.log(`Server running inn ${process.env.NODE_ENV} mode on port ${PORT}`))
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
