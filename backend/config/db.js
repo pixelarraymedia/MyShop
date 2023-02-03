e5a3305be8d4b3525dbb9584532eb43ccd24cc69
Darion <pixelarraymedia@gmail.com>
2023-02-03 15:23:05 -0800

    update : change to connect to db code

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,6 +5,9 @@ const connectDB = async () => {
 
     try {
         const conn = await mongoose.connect(process.env.MONGO_URI, {
+            useUnifiedTopology: true,
+            useNewUrlParser: true,
+            useCreateIndex: true,
         })
 
         console.log(`MongoDB connected: ${conn.connection.host}` .cyan.underline)
