deb6878428e945d3aeddeb02af7c5b288fe80cb0
Darion <pixelarraymedia@gmail.com>
2023-05-05 13:49:01 -0700

    added limiter for security

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -11,10 +11,19 @@ authUser,
   updateUser
 } from '../controllers/userController.js'
 import { protect, admin } from '../middleware/authMiddleware.js'
+import { rateLimit } from 'express-rate-limit'
+
+ const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 5,
+  message: "Too many login attempts, please try again later"
+ });
 
 
 router.route('/').post(registerUser).get(protect, admin, getUsers)
-router.post('/login', authUser)
+router.post('/login', limiter , authUser, (req, res) => {
+
+}); 
 router.route('/profile').get(protect, getUserProfile).put(protect, updateUserProfile)
 router.route('/:id').delete(protect, admin, deleteUser).get(protect, admin, getUserById).put(protect, admin, updateUser)
 
